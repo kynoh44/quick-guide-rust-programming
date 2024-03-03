73853c82f0484d331b58056a38b967ff4a6d15f2
Gioh Kim <gurugio@gmail.com>
2024-03-03 11:15:52 +0900

    Add first example
    Add source and how to run it

--- src/function_for/main.rs
+++ src/function_for/main.rs
@@ -1,3 +1,21 @@
+fn fizzbuzz_if_else(max: i32) {
+    for i in 1..=max {
+        let rem_three: i32 = i % 3;
+        let rem_five: i32 = i % 5;
+
+        if rem_three == 0 && rem_five == 0 {
+            println!("{} - FizzBuzz", i);
+        } else if rem_three == 0 {
+            println!("{} - Fizz", i);
+        } else if rem_five == 0 {
+            println!("{} - Buzz", i);
+        } else {
+            /* do nothing */
+        }
+    }
+}
+
 fn main() {
     println!("Hello, function_for!");
+    fizzbuzz_if_else(10);
 }
