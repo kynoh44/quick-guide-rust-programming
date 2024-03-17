7b95fb11ea800ec4ae3ceedd906e2d248048827e
Gioh Kim <gurugio@l.com>
2024-03-17 23:41:51 +0900

    basic: array expr match

---
+++ src/expr/main.rs
@@ -0,0 +1,33 @@
+fn ret_zero() -> i32 {
+    0
+}
+
+fn main() {
+    let age = 44;
+    let gen = match age {
+        0..=20 => "MZ",
+        21..=50 => "X",
+        51..=100 => "A",
+        _ => "?",
+    };
+
+    let num = 45;
+    let var = if num % 3 == 0 {
+        3
+    } else {
+        if num % 5 == 0 {
+            5
+        } else {
+            0
+        }
+    };
+
+    let x = 9;
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+
+        // This expression will be assigned to `y`
+        x_cube + x_squared + x
+    };
+}
