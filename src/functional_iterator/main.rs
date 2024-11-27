758cb1016b3e4aed652d4101a4626b06c295be97
Gioh Kim <gurugio@l.com>
2024-11-27 09:08:03 +0900

    functional_iterator: exercise

--- src/functional_iterator/main.rs
+++ src/functional_iterator/main.rs
@@ -1,7 +1,17 @@
 struct Fibonacci {
     curr: u32,
     next: u32,
+    // Solution
+    // buffer: Vec<u32>,
+    // counter: usize,
 }
+
+// Solution
+/*impl Fibonacci {
+    fn nth(&self, index: usize) -> u32 {
+        self.buffer[index]
+    }
+}*/
 
 impl Iterator for Fibonacci {
     type Item = u32;
@@ -12,6 +22,9 @@ impl Iterator for Fibonacci {
         self.curr = self.next;
         self.next = current + self.next;
 
+        // Solution
+        /*self.buffer.push(current);
+        self.counter += 1;*/
         Some(current)
     }
 }
@@ -28,5 +41,30 @@ fn main() {
             // try again after removing this break condition
             break;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_functional_iterator() {
+        let mut fib_iter = Fibonacci { curr: 0, next: 1 };
+
+        // Solution
+        /*let mut fib_iter = Fibonacci {
+            curr: 0,
+            next: 1,
+            buffer: Vec::new(),
+            counter: 0,
+        };*/
+
+        for _ in 1..=10 {
+            let _ = fib_iter.next().unwrap();
+        }
+
+        // Exercise
+        //assert_eq!(5, fib_iter.nth(5));
     }
 }
