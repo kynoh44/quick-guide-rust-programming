a806a2b243bada55c64b59968661b6488efc9c1f
Gioh Kim <gurugio@l.com>
2024-11-28 14:24:34 +0900

    functional: map, filter

---
+++ src/map_inspect/main.rs
@@ -0,0 +1,18 @@
+fn fizzbuzz_3(max: i32) {
+    let ret = (1..=max)
+        .into_iter()
+        .map(|i| match (i % 3, i % 5) {
+            (0, 0) => format!("{} - FizzBuzz\n", i),
+            (0, _) => format!("{} - Fizz\n", i),
+            (_, 0) => format!("{} - Buzz\n", i),
+            (_, _) => "".to_string(),
+        })
+        .inspect(|s| println!("map returns '{}'", s))
+        .collect::<Vec<String>>()
+        .join("");
+    println!("{}", ret);
+}
+
+fn main() {
+    fizzbuzz_3(41);
+}
