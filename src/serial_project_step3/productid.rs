3deee1beb146b14e3fbda8cf4885ba00692a2eaa
Gioh Kim <gurugio@a.com>
2025-01-12 01:33:51 +0900

    WIP: serial_project_step3

--- src/serial_project_step3/productid.rs
+++ src/serial_project_step3/productid.rs
@@ -1,5 +1,4 @@
-use crate::util::get_user_input;
-use crate::GenSerialData;
+use crate::{get_user_input, GenSerialData};
 
 pub struct ProductID {
     pub digit: u32,
@@ -18,7 +17,7 @@ impl GenSerialData for ProductID {
         self.id = Some(get_user_input());
     }
 
-    fn generate(&self) -> Option<&str> {
+    fn get_data(&self) -> Option<&str> {
         self.id.as_ref().map(|x| x.as_str())
     }
 }
--- text/09_tiny_project/09_03_command_option.md
+++ text/09_tiny_project/09_03_command_option.md
@@ -1,3 +1,13 @@
 ## 옵션 추가
 
 각 입력 데이터를 옵션으로 지정
+
+main에서 모든 입력 데이터를 초기화
+명령 인자로 입력 데이터 선택:
+```
+$ serial --help
+모든 입력 데이터의 help 메세지 출력
+$ serial generate --userid 1234 --productid qwerasdf
+..생성
+$ serial verify [serial-key] --userid
+```
