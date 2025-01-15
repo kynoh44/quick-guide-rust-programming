025e25febefcc9006ec720eba5ed4a2da7af2aac
Gioh Kim <gurugio@a.com>
2025-01-16 00:34:49 +0900

    Refine source of serial_project_step2

--- src/serial_project_step2/expiredate.rs
+++ src/serial_project_step2/expiredate.rs
@@ -1,21 +1,17 @@
 use crate::get_user_input;
-use crate::{GenSerialData, InputData};
+use crate::GenSerialData;
 
 pub struct ExpireDate {
-    pub data: InputData, // 공통 데이터는 InputData 구조체로 묶어서 관리
     year: u32,
     month: u32,
     day: u32,
+    name: String,
 }
 
 impl ExpireDate {
     pub fn new(digit: usize) -> Self {
         ExpireDate {
-            data: InputData {
-                name: "ExpireDate".to_owned(),
-                digit,
-                rawdata: None,
-            },
+            name: "ExpireDate".to_owned(),
             year: 0,
             month: 0,
             day: 0,
@@ -24,13 +20,10 @@ impl ExpireDate {
 }
 
 impl GenSerialData for ExpireDate {
-    fn return_input_data(&mut self) -> &mut InputData {
-        &mut self.data
-    }
-
     fn get_input_from_user(&mut self) {
-        println!("Please input the expiration date (YYYY/MM/DD) : ",);
+        println!("Please input the expiration date (YYYY/MM/DD) (e.g. 2025/01/23) : ",);
         let rawdata = get_user_input();
+        assert_eq!(rawdata.len(), 10); // 입력받은 데이터의 길이가 10인지 검증, YYYYMMDD에 /가 2개 들어가므로 10개임
 
         // 입력받은 날짜를 분리해서 year, month, day 필드에 저장
         // 동시에 year, month, day 필드에 저장된 값이 올바른지 검증
@@ -47,8 +40,25 @@ impl GenSerialData for ExpireDate {
             self.day >= 1 && self.day <= 31,
             "The day must be between 1 and 31."
         );
+    }
 
-        let inputdata = self.return_input_data();
-        inputdata.rawdata = Some(rawdata);
+    fn verify(&mut self, data: &str) -> bool {
+        let year = data[0..4].parse().unwrap();
+        let month = data[4..6].parse().unwrap();
+        let day = data[6..8].parse().unwrap();
+
+        self.year == year && self.month == month && self.day == day
+    }
+
+    fn get_length(&mut self) -> usize {
+        8
+    }
+
+    fn get_rawdata(&self) -> String {
+        format!("{:04}{:02}{:02}", self.year, self.month, self.day)
+    }
+
+    fn get_name(&self) -> String {
+        self.name.clone()
     }
 }
