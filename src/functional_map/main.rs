adadb3b1fec03c8fea900472d912e014401b6737
Gioh Kim <gurugio@l.com>
2025-02-11 10:53:07 +0900

    functional_map: remove duplicated code

--- src/functional_map/main.rs
+++ src/functional_map/main.rs
@@ -11,7 +11,6 @@ fn fizzbuzz_2(max: i32) {
 
 fn fizzbuzz_3(max: i32) {
     let ret = (1..=max)
-        .into_iter()
         .map(|i| match (i % 3, i % 5) {
             (0, 0) => format!("{} - FizzBuzz\n", i),
             (0, _) => format!("{} - Fizz\n", i),
